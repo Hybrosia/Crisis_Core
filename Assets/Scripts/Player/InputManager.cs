a5df3c374ab4fdb346a4b899bab68235585b1e4a
Markus Lange <markus@gamedevteacher.com>
2025-01-10 20:53:55 +0100

    Added Jump to Input

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -7,6 +7,10 @@ public class InputManager : MonoBehaviour
     public static Vector2 Move { get; private set; }
     public static Vector2 Look { get; private set; }
     public static bool Sprint { get; private set; }
+    public static bool JumpPressed { get; private set; }
+    public static bool JumpReleased { get; private set; }
+    public static bool JumpHeld { get; private set; } // This is used for Gliding
+                                                      // (When Jump is held while in air you start gliding)
     
 
     private void Update()
@@ -14,6 +18,9 @@ public class InputManager : MonoBehaviour
         Move = _inputSystem.Player.Move.ReadValue<Vector2>();
         Look = _inputSystem.Player.Look.ReadValue<Vector2>();
         Sprint = _inputSystem.Player.Sprint.IsPressed();
+        JumpPressed = _inputSystem.Player.Jump.WasPressedThisFrame();
+        JumpReleased = _inputSystem.Player.Jump.WasReleasedThisFrame();
+        JumpHeld = _inputSystem.Player.Jump.IsPressed();
     }
 
     private void Awake()
