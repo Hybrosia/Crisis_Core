147c5c76d37d73b42b0d0891cb469ba0ee93d5b3
Nils <nilsmorkstene@gmail.com>
2025-08-19 12:37:54 +0200

    Endringer i pausemeny

--- Assets/Scripts/UI/MenuFunctions.cs
+++ Assets/Scripts/UI/MenuFunctions.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuFunctions : MonoBehaviour
 {
     public void ShowSettings()
     {
         SettingsController.ShowSettings();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     
     public void Quit()
